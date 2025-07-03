54974586364558a5e5660c62eb33520e0c5c829e
ismail hassan <ismailshariff350@gmail.com>
2025-07-03 16:57:01 -0500

    all terminology declared

--- projects/typescript/build/js/main.js
+++ projects/typescript/build/js/main.js
@@ -1,3 +1,16 @@
 "use strict";
-let username = "ismail";
-console.log(username);
+let myName = 'ismail';
+let meaningOfLife;
+let isloading;
+let album;
+;
+myName = 'shariff';
+meaningOfLife = 42;
+isloading = true;
+album = 'true';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- projects/typescript/src/main.ts
+++ projects/typescript/src/main.ts
@@ -1,2 +1,18 @@
-let username = "ismail";
-console.log(username);
+let myName: string = 'ismail';
+let meaningOfLife: number;
+let isloading : boolean;
+let album: string | number;;
+
+
+myName = 'shariff';
+meaningOfLife = 42;
+isloading = true;
+album = 'true';
+
+const sum = (a: number, b: string) => {
+  return a + b; 
+}
+let postId : string | number
+let isActive: number | boolean 
+ 
+let re: RegExp = /\w+/g;
