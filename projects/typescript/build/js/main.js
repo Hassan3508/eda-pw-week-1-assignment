04d996e4138e8686c3e262aeeace4fea7010b68a
ismail hassan <ismailshariff350@gmail.com>
2025-07-05 15:18:52 -0500

    covered array onjects and enums

--- projects/typescript/build/js/main.js
+++ projects/typescript/build/js/main.js
@@ -1,16 +1,44 @@
 "use strict";
-let myName = 'ismail';
-let meaningOfLife;
-let isloading;
-let album;
-;
-myName = 'shariff';
-meaningOfLife = 42;
-isloading = true;
-album = 'true';
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'two', 'three'];
+let guiltars = ['Gibson', 'Fender', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'johnny';
+stringArr.push('jimmy');
+guiltars[0] = 1984;
+guiltars.unshift('Ibanez');
+guiltars = stringArr;
+mixedData = guiltars;
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let myTuple = ['EVH', 1984, true];
+let mixed = ['EV', 198, true];
+mixed = myTuple;
+myTuple[1] = 5150;
+// objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+const exampleObj = {
+    prop1: 'David',
+    prop2: 'true',
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+exampleObj.prop1 = 'Eddie';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: ['Van Halen', 1984, '5150']
+};
+let jp = {
+    name: 'Jimmy Page',
+    albums: ['1', '2', 'IV']
+};
+const greetGuiltarsit = (guiltarsit) => {
+    if (guiltarsit.name) {
+        return `Hello ${guiltarsit.name.toUpperCase()}!`;
+    }
+    return 'Hello World!';
+};
+console.log(greetGuiltarsit(jp));
--- projects/typescript/src/main.ts
+++ projects/typescript/src/main.ts
@@ -1,18 +1,67 @@
-let myName: string = 'ismail';
-let meaningOfLife: number;
-let isloading : boolean;
-let album: string | number;;
+let stringArr = ['one', 'two', 'three'];
 
+let guiltars = ['Gibson', 'Fender', 5150];
 
-myName = 'shariff';
-meaningOfLife = 42;
-isloading = true;
-album = 'true';
+let mixedData = ['EVH', 1984, true];
 
-const sum = (a: number, b: string) => {
-  return a + b; 
+stringArr[0] = 'johnny';
+stringArr.push('jimmy');
+
+guiltars[0] =  1984;
+guiltars.unshift('Ibanez');
+guiltars = stringArr;
+mixedData = guiltars;
+
+let test =  []
+let bands: string[] = [];
+bands.push('Van Halen');
+
+// Tuple
+let myTuple: [string, number, boolean] = ['EVH', 1984, true];
+
+let mixed = ['EV', 198, true];
+
+mixed = myTuple;
+myTuple[1] = 5150;
+
+// objects
+let myObj: object;
+myObj = []
+console.log(typeof myObj);
+
+myObj = bands;
+
+const exampleObj = {
+  prop1: 'David',
+  prop2: 'true',
 }
-let postId : string | number
-let isActive: number | boolean 
- 
-let re: RegExp = /\w+/g;
+
+exampleObj.prop1 = 'Eddie';
+
+type Guiltarist = {
+  name: string;
+  active?: boolean;
+  albums: (string | number)[]
+}
+
+  let evh: Guiltarist = {
+  name: 'Eddie',
+  active: false,
+  albums: ['Van Halen', 1984, '5150']
+}
+
+let jp: Guiltarist = {
+  name: 'Jimmy Page',
+  albums: ['1', '2', 'IV']
+}
+
+const greetGuiltarsit = (guiltarsit: Guiltarist) => {
+  if (guiltarsit.name) {
+     return `Hello ${guiltarsit.name.toUpperCase()}!`;
+  }
+  return 'Hello World!';
+  
+}
+
+console.log(greetGuiltarsit(jp));
+
